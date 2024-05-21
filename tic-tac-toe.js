b742f134c7a28c391e1bf896b867482fd2f2e688
Robert McCreary Mannino <fooyinfighter@gmail.com>
2024-05-21 15:02:24 -0700

    simplify tic tac toe

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,37 +1,26 @@
 document.write("<h1>Lets play tic tac toe</h1>");
 
-let startingSearchParams = "?r1c1=-&r1c2=-&r1c3=-&r2c1=-&r2c2=-&r2c3=-&r3c1=-&r3c2=-&r3c3=-"
+let startingSearchParams = "?board=---------"
 
 document.write(`<p> Click on this <a href="${startingSearchParams}">link</a> to start your game </p>`);
 
 let params = new URLSearchParams(document.location.search);
-let r1c1 = params.get("r1c1");
-let r1c2 = params.get("r1c2");
-let r1c3 = params.get("r1c3");
-let r2c1 = params.get("r2c1");
-let r2c2 = params.get("r2c2");
-let r2c3 = params.get("r2c3");
-let r3c1 = params.get("r3c1");
-let r3c2 = params.get("r3c2");
-let r3c3 = params.get("r3c3");
 
-let board = [
-    r1c1, r1c2, r1c3,
-    r2c1, r2c2, r2c3,
-    r3c1, r3c2, r3c3,
-]
+// Board is a string
+let b = params.get("board");
 
-document.write(r1c1 + r1c2 + r1c3);
+
+document.write(b[0] + b[1] + b[2]);
 document.write("<br>");
-document.write(r2c1 + r2c2 + r2c3);
+document.write(b[3] + b[4] + b[5]);
 document.write("<br>");
-document.write(r3c1 + r3c2 + r3c3);
+document.write(b[6] + b[7] + b[8]);
 document.write("<br>");
 
 
-if(board[0] === "X" && board[1] === "X" && board[2] === "X") {
+if(b[0] === "X" && b[1] === "X" && b[2] === "X") {
     document.write("X wins!")
-} else if(board[0] === "Y" && board[1] === "Y" && board[2] === "Y") {
+} else if(b[0] === "Y" && b[1] === "Y" && b[2] === "Y") {
     document.write("Y wins!")
 }
 // TODO: finish the conditions under which each side could win.
